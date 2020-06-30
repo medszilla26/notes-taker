cd52ddd3b1ba09baf3d4ae17778f4577e9dc6545
M3D5 <pablohdzjr@gmail.com>
2020-06-29 23:54:55 -0500

    stopping point

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 //dependencies
+
+var express = require("express");
+var path = require("path");
 const fs = require("fs");
-const express = require("express");
-const path = require("path");
 const { uuid } = require("uuidv4");
 
 var app = express();
 
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 8080;
 
 var data = fs.readFileSync("db.json");
 var notes = JSON.parse(data);
@@ -17,21 +18,21 @@ app.use(express.static("public"));
 
 //html routes
 
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
+app.get("/notes", function (request, respond) {
+  respond.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("/api/notes", function (req, res) {
-  res.json(notes);
+app.get("/api/notes", function (request, respond) {
+  respond.json(notes);
 });
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get("*", function (request, respond) {
+  respond.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 //adds note
-app.post("/api/notes", function (req, res) {
-  var addNote = req.body;
+app.post("/api/notes", function (request, respond) {
+  var addNote = request.body;
 
   addNote.id = uuid();
   notes.push(addNote);
@@ -42,13 +43,13 @@ app.post("/api/notes", function (req, res) {
       }
       console.log("Note Added!");
     });
-    res.json(addNote);
+    respond.json(addNote);
   }
 });
 
 //deletes note
-app.delete("/api/notes/:id", function (req, res) {
-  var id = req.params.id;
+app.delete("/api/notes/:id", function (request, respond) {
+  var id = request.params.id;
 
   notes = notes.filter((note) => {
     return note.id != id;
@@ -60,10 +61,10 @@ app.delete("/api/notes/:id", function (req, res) {
     }
     console.log("Note Deleted!");
   });
-  res.json(notes);
+  respond.json(notes);
 });
 
-//displays port connection on terminal
+//starts server and displays port connection on terminal
 app.listen(PORT, function () {
   console.log("App is listening on PORT " + PORT);
 });
